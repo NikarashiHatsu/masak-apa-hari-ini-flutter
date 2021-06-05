4717443455b58f81ed0205b3c5ff626ff2c3d239
NikarashiHatsu <yourlovelydev@gmail.com>
2021-06-05 11:00:28 +0700

    chore: setting up drawer

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -31,17 +31,71 @@ class _MainScreenState extends State<MainScreen> {
         ],
       ),
       drawer: Drawer(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('This is the Drawer'),
-              ElevatedButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('Close Drawer'),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 1.0,
+                    blurRadius: 1.0,
+                  )
+                ],
               ),
-            ],
-          ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Column(),
+                  IconButton(
+                    icon: Icon(Icons.close),
+                    tooltip: 'Tutup Drawer',
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text('Beranda'),
+              leading: Icon(Icons.home),
+              onTap: () {
+                print('Ke Beranda');
+              },
+            ),
+            ListTile(
+              title: Text('Resep'),
+              leading: Icon(Icons.receipt_long),
+              onTap: () {
+                print('Ke Resep');
+              },
+            ),
+            ListTile(
+              title: Text('Kategori'),
+              leading: Icon(Icons.category),
+              onTap: () {
+                print('Ke Kategori');
+              },
+            ),
+            ListTile(
+              title: Text('Artikel'),
+              leading: Icon(Icons.feed),
+              onTap: () {
+                print('Ke Artikel');
+              },
+            ),
+            ListTile(
+              title: Text('Favorit Saya'),
+              leading: Icon(Icons.bookmarks),
+              onTap: () {
+                print('Ke Favorit Saya');
+              },
+            ),
+          ],
         ),
       ),
     );
