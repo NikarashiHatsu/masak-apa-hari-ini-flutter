1f91e65562a4ca00a759875a39ee8c72300e5b57
NikarashiHatsu <yourlovelydev@gmail.com>
2021-06-10 22:16:20 +0700

    chore: add category page

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,11 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class Category {
-  String? category;
-  String? url;
-  String? key;
+  String category;
+  String url;
+  String key;
 
   Category({
-    this.category,
-    this.url,
-    this.key,
+    required this.category,
+    required this.url,
+    required this.key,
   });
+}
+
+Future<List<Category>> getCategories(http.Client client) async {
+  final response = await client.get(Uri.parse(
+      "https://masak-apa.tomorisakura.vercel.app/api/categorys/recipes"));
+
+  List<dynamic> data = jsonDecode(response.body)['results'];
+
+  return data.map((json) {
+    return Category(
+      category: json['category'],
+      url: json['url'],
+      key: json['key'],
+    );
+  }).toList();
 }
--- lib/pages/kategori.dart
+++ lib/pages/kategori.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:masak_apa_hari_ini/model/category.dart';
 
-class Kategori extends StatelessWidget {
+class Kategori extends StatefulWidget {
   const Kategori({Key? key}) : super(key: key);
 
   @override
+  _KategoriState createState() => _KategoriState();
+}
+
+class _KategoriState extends State<Kategori> {
+  bool _isErrorButtonReloading = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Kategori'),
+    return FutureBuilder(
+      future: getCategories(Client()),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.hasError) {
+          _isErrorButtonReloading = false;
+
+          return Column(
+            children: <Widget>[
+              Text('Terjadi kesalahan saat mengambil categori resep.'),
+              SizedBox(height: 8.0),
+              MaterialButton(
+                child: _isErrorButtonReloading
+                    ? Text('Memuat ulang...')
+                    : Text('Muat ulang'),
+                color: Colors.blue,
+                disabledColor: Colors.grey,
+                textColor: Colors.white,
+                onPressed: _isErrorButtonReloading
+                    ? null
+                    : () {
+                        _isErrorButtonReloading = true;
+                        setState(() {});
+                      },
+              )
+            ],
+          );
+        }
+
+        return snapshot.hasData
+            ? CategoryListBuilder(category: snapshot.data)
+            : Text('Tidak ada kategori resep yang dapat ditampilkan.');
+      },
+    );
+  }
+}
+
+class CategoryListBuilder extends StatelessWidget {
+  const CategoryListBuilder({Key? key, required this.category})
+      : super(key: key);
+  final List<Category> category;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: category.length,
+      physics: BouncingScrollPhysics(),
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(
+          height: 0,
+        );
+      },
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          key: Key(category[index].key),
+          title: Text(category[index].category),
+          onTap: () {
+            // TODO: Buat redirection ke list category
+            print(category[index].url);
+          },
+        );
+      },
     );
   }
 }
