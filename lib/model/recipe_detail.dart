f5aa9c7c3d99f5b145ebdf0b3883996b5268df81
NikarashiHatsu <yourlovelydev@gmail.com>
2021-07-28 19:50:49 +0700

    chore: revamp recipe_detail model

--- lib/model/recipe_detail.dart
+++ lib/model/recipe_detail.dart
@@ -1,20 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:masak_apa_hari_ini/model/author.dart';
+
 class RecipeDetail {
   String? title;
   String? thumb;
-  String? portion;
-  String? time;
+  String? servings;
+  String? times;
   String? difficulty;
-  Map<String, String>? author;
+  Author? author;
   String? desc;
-  List<Map<String, String>>? needItem;
-  List<String>? ingredients;
-  List<String>? steps;
+  List<dynamic>? needItem;
+  List<dynamic>? ingredients;
+  List<dynamic>? steps;
 
   RecipeDetail({
     this.title,
     this.thumb,
-    this.portion,
-    this.time,
+    this.servings,
+    this.times,
     this.difficulty,
     this.author,
     this.desc,
@@ -22,4 +26,29 @@ class RecipeDetail {
     this.ingredients,
     this.steps,
   });
+}
+
+Future<RecipeDetail> getRecipeDetail(http.Client client, String recipeKey) async {
+  final response = await client
+    .get(Uri.parse("https://masak-apa.tomorisakura.vercel.app/api/recipe/$recipeKey"));
+
+  Map<dynamic, dynamic> data = jsonDecode(response.body)['results'];
+
+  Author author = new Author(
+    user: data['author']['user'],
+    datePublished: data['author']['datePublished'],
+  );
+
+  return RecipeDetail(
+    author: author,
+    desc: data['desc'],
+    difficulty: data['dificulty'],
+    ingredients: data['ingredient'],
+    needItem: data['needItem'],
+    servings: data['servings'],
+    steps: data['step'],
+    thumb: data['thumb'] ?? 'https://via.placeholder.com/500x350',
+    times: data['times'],
+    title: data['title']
+  );
 }
